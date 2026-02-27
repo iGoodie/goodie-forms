b9e0d6be283608efb0a86c4a5ae5b37828de6bea
Taha Anılcan Metinyurt <igoodie@programmer.net>
2026-02-27 23:29:49 +0300

    Updates

---
+++ assets/TGqfAeTB.js
@@ -0,0 +1,1 @@
+import{r as t}from"./S_eiYIyL.js";import e from"./Cds7ZdTc.js";import{$ as n,b as s,o as a,v as i,y as p}from"./DvHdMPVA.js";import"./BN_7HF1G.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
