aee8d170a1351f1e8b71b10f30d44ba9aa187772
Taha Anılcan Metinyurt <igoodie@programmer.net>
2026-02-27 23:34:55 +0300

    Updates

---
+++ assets/B9Tby2mx.js
@@ -0,0 +1,1 @@
+import{r as s}from"./BJXTRIno.js";import t from"./orJ_2Em_.js";import{$ as e,b as a,o as p,v as c,y as n}from"./jBKG_fey.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ assets/DXr-1i21.js
@@ -0,0 +1,1 @@
+import{r as t}from"./BJXTRIno.js";import a from"./orJ_2Em_.js";import{$ as e,b as n,o as s,v as c,y as i}from"./jBKG_fey.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
