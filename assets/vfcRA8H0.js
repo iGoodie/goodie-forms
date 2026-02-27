671c96e5c7f9c43fcad2c5fb1f684ee743c32bbd
Taha Anılcan Metinyurt <igoodie@programmer.net>
2026-02-27 23:25:56 +0300

    Updates

--- assets/vfcRA8H0.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./S_eiYIyL.js";import a from"./Cds7ZdTc.js";import{$ as e,b as n,o as s,v as c,y as i}from"./DvHdMPVA.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
