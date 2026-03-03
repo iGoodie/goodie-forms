5c33f8709420d24bfc266c28b7cce8b2356b9540
Taha Anılcan Metinyurt <igoodie@programmer.net>
2026-03-03 23:00:05 +0300

    Updates

---
+++ assets/CKpyLiS6.js
@@ -0,0 +1,1 @@
+import{r as s}from"./BIP2O5pw.js";import t from"./BN6F6QVe.js";import{$ as e,b as a,o as p,v as c,y as n}from"./CdNahCnE.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ assets/DohY0KrJ.js
@@ -0,0 +1,1 @@
+import{r as t}from"./BIP2O5pw.js";import a from"./BN6F6QVe.js";import{$ as e,b as n,o as s,v as c,y as i}from"./CdNahCnE.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
